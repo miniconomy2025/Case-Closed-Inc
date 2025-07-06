b5cee02be2ec50c8776bb834e1e8a744c2663177
Cindi-Ann Jeffery <cindi-anne.jeffery@bbd.co.za>
2025-07-06 19:45:49 +0200

    added acm cert

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -20,11 +20,17 @@ provider "aws" {
   region = var.aws_region
 }
 
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 resource "aws_acm_certificate" "main" {
-  provider                  = aws.us_east_1
-  domain_name              = "case-supplier.projects.bbdgrad.com"
+  provider                   = aws.us_east_1
+  domain_name                = "case-supplier.projects.bbdgrad.com"
   subject_alternative_names = ["case-supplier-api.projects.bbdgrad.com"]
-  validation_method        = "DNS"
+  validation_method          = "DNS"
 
   lifecycle {
     create_before_destroy = true
