adee5028b5465f167c8cc6fd564e327c7b63980d
Liam Barkley <Liam.Barkley@bbd.co.za>
2025-07-08 15:22:56 +0200

    accounted for multiple pickups

--- server/controllers/logisticsController.js
+++ server/controllers/logisticsController.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import { getExternalOrderWithItems } from '../daos/externalOrdersDao.js';
 import { decrementStockByName, incrementStockByName } from '../daos/stockDao.js';
-import { getCaseOrderById, updateCaseOrderStatus } from '../daos/caseOrdersDao.js';
+import { getCaseOrderById, updateCaseOrderStatus, incrementQuantityDelivered } from '../daos/caseOrdersDao.js';
 import { getOrderStatusByName } from '../daos/orderStatusesDao.js';
 
 /**
@@ -26,6 +26,7 @@ export const handleLogistics = async (req, res, next) => {
                 return res
                     .status(StatusCodes.OK)
                     .json({ message: 'Successfully received external order' });
+
             case 'PICKUP':
                 const order = await getCaseOrderById(id);
                 if (!order) {
@@ -39,13 +40,25 @@ export const handleLogistics = async (req, res, next) => {
                         .status(StatusCodes.BAD_REQUEST)
                         .json({ error: 'Payment has not been received for order.' });
                 };
+
+                if (quantity + order.quantity_delivered > order.quantity) {
+                    return res
+                        .status(StatusCodes.BAD_REQUEST)
+                        .json({ error: 'Requested quantity exceeded for order id. ' });
+                }
+
                 // revoke stock after pickup
-                await decrementStockByName('case', order.quantity);
-                const completeStatus = await getOrderStatusByName('order_complete');
-                await updateCaseOrderStatus(id, completeStatus.id);
+                await decrementStockByName('case', quantity);
+                const updatedOrder = await incrementQuantityDelivered(id, quantity)
+
+                if (updatedOrder.quantity < updatedOrder.quantity_delivered) {
+                    const completeStatus = await getOrderStatusByName('order_complete');
+                    await updateCaseOrderStatus(id, completeStatus.id);
+                }
+
                 return res
                     .status(StatusCodes.OK)
-                    .json({ message: 'Order status updated to order_complete and stock reduced.' });
+                    .json({ message: 'Successfully notified of pickup' });
             default:
                 return res
                     .status(StatusCodes.BAD_REQUEST)
--- server/daos/caseOrdersDao.js
+++ server/daos/caseOrdersDao.js
@@ -50,6 +50,12 @@ export const incrementAmountPaid = async (orderId, amount) => {
     .increment('amount_paid', amount);
 };
 
+export const incrementQuantityDelivered = async (orderId, quantity) => {
+  return db(TABLE_NAME)
+    .where({ id: orderId })
+    .increment('quantity_delivered', quantity);
+};
+
 export const updateOrderAccountNumber = async (orderId, accountNumber) => {
   return db(TABLE_NAME)
     .where({ id: orderId })
