044a7458dff624756c8dce26941a6ad67c55f62b
Chris McHardy <chris.mchardy@bbd.co.za>
2025-07-08 14:46:30 +0200

    Add ordered_units to stock and accounted for it on delivery

--- server/controllers/logisticsController.js
+++ server/controllers/logisticsController.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import { getExternalOrderWithItems } from '../daos/externalOrdersDao.js';
-import { decrementStockByName, incrementStockByName } from '../daos/stockDao.js';
+import { decrementStockByName, deliverStockByName } from '../daos/stockDao.js';
 import { getCaseOrderById, updateCaseOrderStatus } from '../daos/caseOrdersDao.js';
 import { getOrderStatusByName } from '../daos/orderStatusesDao.js';
 
@@ -21,8 +21,7 @@ export const handleLogistics = async (req, res, next) => {
                         .status(StatusCodes.NOT_FOUND)
                         .json({ error: 'Delivery order not found' });
                 };
-                console.log(deliveryOrder);
-                await incrementStockByName(deliveryOrder.stock_type_name, quantity);
+                await deliverStockByName(deliveryOrder.stock_type_name, quantity);
                 return res
                     .status(StatusCodes.OK)
                     .json({ message: 'Successfully received external order' });
