1473fb12b8e4716d51460d92477523660a7fa847
Liam Barkley <Liam.Barkley@bbd.co.za>
2025-10-19 20:45:38 +0200

    fixed check for commercial bank account

--- server/cron/jobs/decisionEngine.js
+++ server/cron/jobs/decisionEngine.js
@@ -59,8 +59,11 @@ export default class DecisionEngine {
     async run() {
         let have_account = false;
         try{
-            await BankClient.getMyAccount();
-            have_account = true;
+            const { accountNumber } = await BankClient.getMyAccount();
+            if (accountNumber) {
+                have_account = true;
+            }
+            
         }catch {
             have_account = false;
         }
@@ -117,7 +120,7 @@ export default class DecisionEngine {
         }else{
             try {
                 const { accountNumber } = await BankClient.createAccount({
-                    notification_url: "https://case-supplier-api.projects.bbdgrad.com/api/payment",
+                    notification_url: process.env.BANK_PAYMENT_URL,
                 });
                 // Store our account number
                 await updateAccount(accountNumber, 0);
