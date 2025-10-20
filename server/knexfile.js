1f3d023870b5dec11d8acb5b90a9f346b182b221
Cindi-Ann Jeffery <cindi-anne.jeffery@bbd.co.za>
2025-10-20 09:36:22 +0200

    setup artillery

--- server/knexfile.js
+++ server/knexfile.js
@@ -9,9 +9,9 @@ let knexEnv = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: {
-      rejectUnauthorized: false
-    },
+    // ssl: {
+    //   rejectUnauthorized: false
+    // },
   },
   migrations: {
     directory: "./migrations",
